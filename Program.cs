29107eb72efb52d51b63ab94f79ae3b7c357c3a8
Druce <C:\Users\gwpc1\AppData\Roaming\The Bat!>
2022-10-12 02:03:34 +0300

    add program code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿int NumberOfStrings(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetStrings(string[] words)
+{
+    for (int i = 0; i < words.Length; i++)
+    {
+        words[i] = Console.ReadLine() ?? "";
+    }
+    Console.WriteLine("Новый массив из строк: ");
+}
+
+void SearchOfStrings(string[] words)
+{
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length <= 3) Console.Write(words[i] + " ");
+    }
+}
+
+int n = NumberOfStrings("Введите количество строк, которые хотите ввести: ");
+string[] words = new string[n];
+Console.WriteLine($"Поочередно введите указаное количество строк ({n}): ");
+GetStrings(words);
+SearchOfStrings(words);
